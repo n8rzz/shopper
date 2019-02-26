15dfccf3dd05c983bbc37d81b267b3d672a99c55
Nate Geslin <teamtomkins23@gmail.com>
2019-02-25 19:53:51 -0600

    updates assemblies#show to display items * updates db seeds with assemblies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-['Carrots', 'Milk', 'Cheese', 'Pop Tarts', 'Root Beer'].each do |item_name|
+item_names = ['Carrots', 'Milk', 'Cheese', 'Pop Tarts', 'Root Beer']
+
+item_names.each do |item_name|
   Item.create(name: item_name)
 end
 
 ['Beef Stew'].each do |meal_name|
-  Assembly.create(name: meal_name)
+  a = Assembly.create(name: meal_name)
+  a.item_ids = [1, 2]
 end
