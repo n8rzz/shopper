5967464f216b939472e639d98eb19e0a38a35d6d
Nate Geslin <teamtomkins23@gmail.com>
2019-02-26 22:10:52 -0600

    updates views to include more association informations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
-  belongs_to :item
-  belongs_to :assembly
+  belongs_to :item, optional: true
+  belongs_to :assembly, optional: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,8 @@ end
   a.item_ids = Item.first.id
 end
 
-order_statuses = ['Pending', 'Active', 'Complete']
-order_statuses.each do |status|
-  o = Order.create(status: status)
+order_statuses = ['pending', 'active', 'complete']
+o = Order.create(status: order_statuses[0])
+o.order_item_ids << OrderItem.create(order_id: Order.first.id, item_id: Item.first.id)
+o.order_item_ids << OrderItem.create(order_id: Order.first.id, assembly_id: Assembly.first.id)
 
-  unless rand(0..1) == 1
-    OrderItem.create(order_id: o.id, assembly_id: Item.first.id)
-  end
-
-  OrderItem.create(order_id: o.id, item_id: Item.first.id)
-end
