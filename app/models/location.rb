1844a44ac60e0f4739438225e3790209e755e45a
Nate Geslin <teamtomkins23@gmail.com>
2019-03-13 19:33:15 -0500

    tightens up views and _forms

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def city_and_state
+    "#{self.city}, #{self.state}"
+  end
 end
