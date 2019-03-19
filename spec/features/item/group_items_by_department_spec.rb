6590b213ad9fc8f3f9a307915d195672f9037482
Nate Geslin <teamtomkins23@gmail.com>
2019-03-18 22:40:46 -0500

    adds groupBy department option to Item#index

---
+++ spec/features/item/group_items_by_department_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Group Items by Department' do
+  let(:produce) { create(:department, name: 'Produce') }
+  let(:meat) { create(:department, name: 'Meat') }
+  let!(:produce_item) { create(:item, department: produce) }
+  let!(:meat_item) { create(:item, department: meat) }
+
+  before do
+    visit items_path
+  end
+
+  it 'has Department link' do
+    expect(page).to have_link 'Department'
+  end
+
+  context 'when clicking on Department link' do
+    it 'Department names are headings' do
+      click_link 'Department'
+
+      expect(page).to have_selector 'h2', text: produce.name
+      expect(page).to have_selector 'h2', text: meat.name
+    end
+  end
+end
