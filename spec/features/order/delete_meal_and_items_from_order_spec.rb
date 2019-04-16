727d8a780f41d1a3b3a40ec668fa28e9150c44bd
Nate Geslin <teamtomkins23@gmail.com>
2019-04-15 23:16:50 -0500

    hides delete meal link for !Orders.live?

--- spec/features/order/delete_meal_and_items_from_order_spec.rb
+++ spec/features/order/delete_meal_and_items_from_order_spec.rb
@@ -5,24 +5,25 @@ RSpec.feature 'Delete meal and items from order', js: true do
   let!(:complete_order) { create(:order, :complete, :with_order_items) }
 
   context 'when an order is live' do
-    it 'removes meal from view' do
+    before :each do
       visit order_path(pending_order.id)
       click_link('Meal')
 
       accept_confirm do
         click_link('Delete')
       end
-
-      expect(page).to have_no_text(pending_order.order_items.first.item.name)
-      expect(page).to have_current_path("/orders/#{pending_order.id}?order=assembly")
     end
+
+    it { expect(page).to have_no_text(pending_order.order_items.first.item.name) }
+    it { expect(page).to have_current_path("/orders/#{pending_order.id}?order=assembly") }
   end
 
   context 'when an order is not live' do
-    it 'no Delete link is present' do
+    before :each do
       visit order_path(complete_order.id)
-
-      expect(page).to have_no_link('Delete')
+      click_link('Meal')
     end
+
+    it { expect(page).to have_no_link('Delete') }
   end
 end
