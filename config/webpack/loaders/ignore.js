a7c2d0369687f1ee64096d0ff46dbec6d886c758
Nate Geslin <teamtomkins23@gmail.com>
2019-07-18 21:57:34 -0500

    updates add to order btn styles to [finally] not shift around on :active * adds ignore loaders to ignore spec.js files during build

---
+++ config/webpack/loaders/ignore.js
@@ -0,0 +1,4 @@
+module.exports = {
+    test: /\.spec\.js$/,
+    loader: 'ignore-loader'
+};
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const ignoreLoader = require('./loaders/ignore');
+environment.loaders.append('ignore', ignoreLoader);
 
 module.exports = environment.toWebpackConfig()
