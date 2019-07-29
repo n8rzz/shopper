5d900795799a76f447606afd86903fe4cc459f3f
Nate Geslin <teamtomkins23@gmail.com>
2019-07-29 18:11:52 -0500

    closes #32 - removes unused boolean property from orders_controller used to show/not show `Not Grouped` heading * adds regression test to cover existence of `Not Grouped` heading

---
+++ spec/features/order/view_an_order_with_groups_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'View an order with groups', js: true do
+  let!(:item) { create(:item) }
+  let!(:pending_order) { create(:order, :pending) }
+
+  context 'when a non-grouped item exists within an order' do
+    before do
+      visit items_path
+      click_button 'Add to order'
+
+      visit order_path(pending_order.id)
+      click_link 'Meal'
+    end
+
+    it { expect(page).to have_text('Not Grouped') }
+  end
+end
