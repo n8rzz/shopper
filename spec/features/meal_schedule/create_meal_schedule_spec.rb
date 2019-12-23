4a74cf9a6f8577c1e22c757fa6044d4139cce88c
Nate Geslin <teamtomkins23@gmail.com>
2019-12-22 20:02:33 -0600

    closes #343 - updates specs to handle both group and non-grouped users interacting with meal_schedule

--- spec/features/meal_schedule/create_meal_schedule_spec.rb
+++ spec/features/meal_schedule/create_meal_schedule_spec.rb
@@ -1,53 +1,108 @@
 require 'rails_helper'
 
 RSpec.feature 'Create a MealSchedule', js: true do
-  let(:user) { create(:user) }
-  let!(:item) { create(:item, ownable: user) }
-  let!(:assembly) { create(:assembly, ownable: user) }
+  context 'as a non-grouped user' do
+    let(:user) { create(:user) }
+    let!(:item) { create(:item, ownable: user) }
+    let!(:assembly) { create(:assembly, ownable: user) }
 
-  before do
-    sign_in user
-  end
-
-  after do
-    sign_out user
-  end
-
-  describe 'with assembly' do
-    before :each do
-      visit meal_schedules_path
-
-      click_link('New Schedule')
-
-      select 'morning', from: 'Meal time'
-      select assembly.name, from: 'Assembly'
-      fill_in 'meal_schedule_schedule_date', with: Time.now
-
-      click_button('Create Meal schedule')
+    before do
+      sign_in user
     end
 
-    it { expect(page).to have_current_path(meal_schedules_path) }
-    it { expect(page).to have_text('MealSchedule was added successfully') }
-    it { expect(page).to have_text(assembly.name) }
-    it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
-  end
-
-  describe 'with item' do
-    before :each do
-      visit meal_schedules_path
-
-      click_link('New Schedule')
-
-      select 'morning', from: 'Meal time'
-      select item.name, from: 'Item'
-      fill_in 'meal_schedule_schedule_date', with: Time.now
-
-      click_button('Create Meal schedule')
+    after do
+      sign_out user
     end
 
-    it { expect(page).to have_current_path(meal_schedules_path) }
-    it { expect(page).to have_text('MealSchedule was added successfully') }
-    # it { expect(page).to have_text(item.name) }
-    it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
+    describe 'with assembly' do
+      before :each do
+        visit meal_schedules_path
+
+        click_link('New Schedule')
+
+        select 'morning', from: 'Meal time'
+        select assembly.name, from: 'Assembly'
+        fill_in 'meal_schedule_schedule_date', with: Time.now
+
+        click_button('Create Meal schedule')
+      end
+
+      it { expect(page).to have_current_path(meal_schedules_path) }
+      it { expect(page).to have_text('MealSchedule was added successfully') }
+      it { expect(page).to have_text(assembly.name) }
+      it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
+    end
+
+    describe 'with item' do
+      before :each do
+        visit meal_schedules_path
+
+        click_link('New Schedule')
+
+        select 'morning', from: 'Meal time'
+        select item.name, from: 'Item'
+        fill_in 'meal_schedule_schedule_date', with: Time.now
+
+        click_button('Create Meal schedule')
+      end
+
+      it { expect(page).to have_current_path(meal_schedules_path) }
+      it { expect(page).to have_text('MealSchedule was added successfully') }
+      it { expect(page).to have_text(item.name) }
+      it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
+    end
+  end
+
+  context 'as a non-grouped user' do
+    let(:user) { create(:user) }
+    let(:group) { create(:group, user_ids: [user.id]) }
+    let!(:item) { create(:item, ownable: group) }
+    let!(:assembly) { create(:assembly, ownable: group) }
+
+    before do
+      sign_in user
+    end
+
+    after do
+      sign_out user
+    end
+
+    describe 'with assembly' do
+      before :each do
+        visit meal_schedules_path
+
+        click_link('New Schedule')
+
+        select 'morning', from: 'Meal time'
+        select assembly.name, from: 'Assembly'
+        fill_in 'meal_schedule_schedule_date', with: Time.now
+
+        click_button('Create Meal schedule')
+      end
+
+      it { expect(page).to have_current_path(meal_schedules_path) }
+      it { expect(page).to have_text('MealSchedule was added successfully') }
+      it { expect(page).to have_text(assembly.name) }
+      it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
+    end
+
+    describe 'with item' do
+      before :each do
+        visit meal_schedules_path
+
+        click_link('New Schedule')
+
+        select 'morning', from: 'Meal time'
+        select item.name, from: 'Item'
+        fill_in 'meal_schedule_schedule_date', with: Time.now
+
+        click_button('Create Meal schedule')
+      end
+
+      it { expect(page).to have_current_path(meal_schedules_path) }
+      it { expect(page).to have_text('MealSchedule was added successfully') }
+      it { expect(page).to have_text(item.name) }
+      it { expect(find(".qa-calendarTable-td-cell-#{Date.parse(Time.now.to_s).day}")) }
+    end
   end
 end
--- spec/features/meal_schedule/delete_meal_schedule_spec.rb
+++ spec/features/meal_schedule/delete_meal_schedule_spec.rb
@@ -1,25 +1,52 @@
 require 'rails_helper'
 
 RSpec.feature 'Delete a MealSchedule', js: true do
-  let(:user) { create(:user) }
-  let(:item) { create(:item, ownable: user) }
-  let!(:meal_schedule) { create(:meal_schedule, item_id: item.id, schedule_date: Time.now, ownable: user) }
+  context 'as a non-grouped user' do
+    let(:user) { create(:user) }
+    let(:item) { create(:item, ownable: user) }
+    let!(:meal_schedule) { create(:meal_schedule, item_id: item.id, schedule_date: Time.now, ownable: user) }
 
-  before do
-    sign_in user
-  end
-
-  after do
-    sign_out user
-  end
-
-  scenario 'from meal_schedule#index' do
-    visit meal_schedules_path
-
-    within first('.mealSchedule-bd-itemList-item') do
-      find('.js-mealSchedule-remove').click
+    before do
+      sign_in user
     end
 
-    expect(page).to_not have_text(meal_schedule.item.name)
+    after do
+      sign_out user
+    end
+
+    scenario 'from meal_schedule#index' do
+      visit meal_schedules_path
+
+      within first('.mealSchedule-bd-itemList-item') do
+        find('.js-mealSchedule-remove').click
+      end
+
+      expect(page).to_not have_text(meal_schedule.item.name)
+    end
+  end
+
+  context 'as a group user' do
+    let(:user) { create(:user) }
+    let(:group) { create(:group, user_ids: [user.id]) }
+    let(:item) { create(:item, ownable: group) }
+    let!(:meal_schedule) { create(:meal_schedule, item_id: item.id, schedule_date: Time.now, ownable: group) }
+
+    before do
+      sign_in user
+    end
+
+    after do
+      sign_out user
+    end
+
+    scenario 'from meal_schedule#index' do
+      visit meal_schedules_path
+
+      within first('.mealSchedule-bd-itemList-item') do
+        find('.js-mealSchedule-remove').click
+      end
+
+      expect(page).to_not have_text(meal_schedule.item.name)
+    end
   end
 end
